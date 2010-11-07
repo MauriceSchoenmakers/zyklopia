30a3e31ed8f5db5ddeeeec3fa8ee8153263b205d
Maurice Schoenmakers <maurice.schoenmakers@hotspot-zone.com>
2010-11-07 14:19:49 +0100

    x

--- server/js/z/server.js
+++ server/js/z/server.js
@@ -1,11 +1,15 @@
 if(typeof void 0===typeof j)var j={module:require('j/module').module};j.module.use_require=true;
-j.module('z.server',['j','node=j.http.node.server','http','net'],function M(){var server=M.z.server,j=M.j,http=M.http,node=M.node;
+j.module('z.server',['j','node=j.http.node','http','net','sys'],function M(){var server=M.z.server,j=M.j,http=M.http,node=M.node,sys=M.sys;
 
+j.log=function(m){M.sys.puts(m);return m;};
+j.log('hello');
 
 // WEB SERVER
-http.createServer(function(K,S){var p=j.f.m.p('p'),s=j.f.m.p('s'),res=j.f.m.p('res'),doc='/Users/maurice_schoenmakers/Documents/workspace/';return S(
-  {m:S.get('z(.*)\\.(.*)'),p:'p$1|s$2',f:j.s(S,S.send_file,[res,doc+'/zyklopia/server/web/js/z',p,s])},
-  {m:S.get('j(.*)\\.(.*)'),p:'p$1|s$2',f:j.s(S,S.send_file,[res,doc+'/j/js/j',p,s])},
+server.data={};
+
+http.createServer(function(K,S){var p=j.f.m.p('p'),s=j.f.m.p('s'),res=j.f.m.p('res'),doc='/home/ubuntu/zyklopia/';return S(
+  {m:S.get('z(.*)\\.(.*)'),p:'p$1|s$2',f:j.s(S,S.send_file,[res,doc+'zyklopia/server/web/js/z',p,s])},
+  {m:S.get('j(.*)\\.(.*)'),p:'p$1|s$2',f:j.s(S,S.send_file,[res,doc+'j/js/j',p,s])},
   {m:S.get('/test'),f:function(m){S.test.html(m);test(m)}},
   {filter:true,m:S.get('(.*)/'),p:'d$1',f:function(m){m.url.pathname=m.d+'/index.html';}},
   {m:S.get('(.*)\\.(.*)'),p:'p$1|s$2',f:j.s(S,S.send_file,[res,doc+'zyklopia/server/web',p,s])}
@@ -21,7 +25,7 @@ function test(m){var log=m.log||j.log;
 
 
 // MEDIA SERVER
-net.createServer(function (socket) {
+/*net.createServer(function (socket) {
   socket.setEncoding("utf8");
   socket.write("Echo server\r\n");
   socket.on("data", function (data) {
@@ -32,6 +36,6 @@ net.createServer(function (socket) {
     socket.end();
   });
 }).listen(9100);
-
+*/
 
 });
